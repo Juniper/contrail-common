Juniper/contrail-sandesh

Adding support for new type uuid in sandesh
Change-Id: I2306fde9eea9c3188ea66c0863dd09162ac0822a

library/c/protocol/thrift_protocol.c
@@ -244,6 +244,13 @@ thrift_protocol_write_xml (ThriftProtocol *protocol,
 }
 
 int32_t
+thrift_protocol_write_uuid (ThriftProtocol *protocol,
+                           const uuid_t value, int *error)
+{
+  return protocol->write_uuid (protocol, value, error);
+}
+
+int32_t
 thrift_protocol_read_message_begin (ThriftProtocol *protocol,
                                     char **name,
                                     ThriftMessageType *message_type,
@@ -443,6 +450,13 @@ thrift_protocol_read_xml (ThriftProtocol *protocol,
 }
 
 int32_t
+thrift_protocol_read_uuid (ThriftProtocol *protocol,
+                          uuid_t *value, int *error)
+{
+  return protocol->read_uuid (protocol, value, error);
+}
+
+int32_t
 thrift_protocol_skip (ThriftProtocol *protocol, ThriftType type, int *error)
 {
   switch (type)
@@ -506,6 +520,11 @@ thrift_protocol_skip (ThriftProtocol *protocol, ThriftType type, int *error)
         u_int32_t ret = thrift_protocol_read_binary (protocol, &data, &len, error);
         os_free (data);
         return ret;
+      }
+    case T_UUID:
+      {
+        uuid_t uuid;
+        return thrift_protocol_read_uuid (protocol, &uuid, error);
       }
     case T_STRUCT:
       {

library/c/protocol/thrift_protocol.h
@@ -24,6 +24,8 @@
 extern "C" {
 #endif
 
+#include<uuid/uuid.h>
+
 /*! \file thrift_protocol.h
  *  \brief Abstract class for Thrift protocol implementations.
  */
@@ -56,6 +58,7 @@ typedef enum {
   T_U32    = 20,
   T_XML    = 21,
   T_IPV4   = 22,
+  T_UUID   = 23,
 } ThriftType;
 
 /**
@@ -137,6 +140,8 @@ struct _ThriftProtocol
                           const u_int32_t len, int *error);
   int32_t (*write_xml) (struct _ThriftProtocol *protocol, const char *str,
                         int *error);
+  int32_t (*write_uuid) (struct _ThriftProtocol *protocol, const uuid_t value,
+                        int *error);
 
   int32_t (*read_sandesh_begin) (struct _ThriftProtocol *protocol, char **name,
                                  int *error);
@@ -178,6 +183,7 @@ struct _ThriftProtocol
   int32_t (*read_binary) (struct _ThriftProtocol *protocol, void **buf,
                           u_int32_t *len, int *error);
   int32_t (*read_xml) (struct _ThriftProtocol *protocol, char **str, int *error);
+  int32_t (*read_uuid) (struct _ThriftProtocol *protocol, uuid_t *value, int *error);
 };
 typedef struct _ThriftProtocol ThriftProtocol;
 
@@ -281,6 +287,9 @@ int32_t thrift_protocol_write_binary (ThriftProtocol *protocol,
 int32_t thrift_protocol_write_xml (ThriftProtocol *protocol,
                                    const char *str, int *error);
 
+int32_t thrift_protocol_write_uuid (ThriftProtocol *protocol,
+                                   const uuid_t value, int *error);
+
 int32_t thrift_protocol_read_message_begin (ThriftProtocol *thrift_protocol,
                                             char **name,
                                             ThriftMessageType *message_type,
@@ -370,6 +379,9 @@ int32_t thrift_protocol_read_binary (ThriftProtocol *protocol,
 
 int32_t thrift_protocol_read_xml (ThriftProtocol *protocol,
                                   char **str, int *error);
+
+int32_t thrift_protocol_read_uuid (ThriftProtocol *protocol,
+                                  uuid_t *value, int *error);
 
 int32_t thrift_protocol_skip (ThriftProtocol *protocol, ThriftType type,
                               int *error);

library/cpp/protocol/TXMLProtocol.cpp
@@ -62,6 +62,7 @@ static const std::string kTypeNameDouble("double");
 static const std::string kTypeNameStruct("struct");
 static const std::string kTypeNameString("string");
 static const std::string kTypeNameXML("xml");
+static const std::string kTypeNameUUID("uuid");
 static const std::string kTypeNameMap("map");
 static const std::string kTypeNameList("list");
 static const std::string kTypeNameSet("set");
@@ -86,7 +87,7 @@ const std::string& TXMLProtocol::fieldTypeName(TType type) {
     case T_U16    : return kTypeNameU16    ;
     case T_U32    : return kTypeNameU32    ;
     case T_U64    : return kTypeNameU64    ;
-    case T_IPV4   : return kTypeNameIPV4    ;
+    case T_IPV4   : return kTypeNameIPV4   ;
     case T_DOUBLE : return kTypeNameDouble ;
     case T_STRING : return kTypeNameString ;
     case T_STRUCT : return kTypeNameStruct ;
@@ -95,6 +96,7 @@ const std::string& TXMLProtocol::fieldTypeName(TType type) {
     case T_LIST   : return kTypeNameList   ;
     case T_SANDESH: return kTypeNameSandesh;
     case T_XML    : return kTypeNameXML    ;
+    case T_UUID   : return kTypeNameUUID   ;
     default: return kTypeNameUnknown;
   }
 }
@@ -168,6 +170,9 @@ TType TXMLProtocol::getTypeIDForTypeName(const std::string &name) {
           break;
         case '6':
           result = T_U64;
+          break;
+        case 'u':
+          result = T_UUID;
           break;
       }
       break;
@@ -641,6 +646,10 @@ int32_t TXMLProtocol::writeXML(const string& str) {
   return writePlain(xmlstr);
 }
 
+int32_t TXMLProtocol::writeUUID(const boost::uuids::uuid& uuid) {
+  const std::string str = boost::uuids::to_string(uuid);
+  return writeString(str);
+}
 /**
  * Reading functions
  */
@@ -1137,6 +1146,18 @@ int32_t TXMLProtocol::readBinary(std::string &str) {
 
 int32_t TXMLProtocol::readXML(std::string &str) {
   return readXMLCDATA(str);
+}
+
+int32_t TXMLProtocol::readUUID(boost::uuids::uuid &uuid) {
+  int32_t ret;
+  std::string str;
+  if ((ret = readXMLString(str)) < 0) {
+    return ret;
+  }
+  std::stringstream ss;
+  ss << str;
+  ss >> uuid;
+  return ret;
 }
 
 }}} // contrail::sandesh::protocol

library/python/pysandesh/protocol/TXMLProtocol.py
@@ -4,6 +4,7 @@
 
 import ctypes
 import re
+import uuid
 from TProtocol import *
 from  pysandesh.sandesh_logger import SandeshLogger
 
@@ -35,6 +36,7 @@ class TXMLProtocol(TProtocolBase):
   _XML_TYPENAME_U64 = 'u64'
   _XML_TYPENAME_IPV4 = 'ipv4'
   _XML_TYPENAME_DOUBLE = 'double'
+  _XML_TYPENAME_UUID = 'uuid'
   _XML_TYPENAME_STRING = 'string'
   _XML_TYPENAME_XML = 'xml'
   _XML_TYPENAME_STRUCT = 'struct'
@@ -66,6 +68,7 @@ class TXMLProtocol(TProtocolBase):
       TType.LIST : self._XML_TYPENAME_LIST,
       TType.SANDESH : self._XML_TYPENAME_SANDESH,
       TType.XML : self._XML_TYPENAME_XML,
+      TType.UUID : self._XML_TYPENAME_UUID,
     }
 
     self._field_type_dict = {}
@@ -288,6 +291,14 @@ class TXMLProtocol(TProtocolBase):
     self.writeBuffer(self._XML_CDATA_OPEN)
     self.writeBuffer(xml)
     self.writeBuffer(self._XML_CDATA_CLOSE)
+    return 0
+
+  def writeUUID(self, uuid):
+    try:
+      self.writeBuffer(str(uuid))
+    except TypeError:
+      self._logger.error('TXML Protocol: Invalid uuid value %s') %(str(uuid))
+      return -1
     return 0
     
   class XMLReader:
@@ -799,6 +810,17 @@ class TXMLProtocol(TProtocolBase):
     if xml is None:
       return (-1, None)
     return (len(xml), xml)      
+
+  def readUUID(self):
+    (uuid_str, length) = self._xml_reader.readXMLValue()
+    if uuid_str is None:
+      return (-1, None)
+    try:
+      uuid_temp = uuid.UUID(uuid_str)
+    except ValueError:
+      self._logger.error('TXML Protocol: Invalid uuid value "%s".' %(uuid_str))
+      return (-1, None)
+    return (length, uuid_temp)
 
 class TXMLProtocolFactory:
   def __init__(self, strictRead=False, strictWrite=True):

Juniper/contrail-common

sandesh/library/c/protocol/thrift_protocol.c
@@ -244,6 +244,13 @@ thrift_protocol_write_xml (ThriftProtocol *protocol,
 }
 
 int32_t
+thrift_protocol_write_uuid_t (ThriftProtocol *protocol,
+                           const uuid_t value, int *error)
+{
+  return protocol->write_uuid_t (protocol, value, error);
+}
+
+int32_t
 thrift_protocol_read_message_begin (ThriftProtocol *protocol,
                                     char **name,
                                     ThriftMessageType *message_type,
@@ -443,6 +450,13 @@ thrift_protocol_read_xml (ThriftProtocol *protocol,
 }
 
 int32_t
+thrift_protocol_read_uuid_t (ThriftProtocol *protocol,
+                          uuid_t *value, int *error)
+{
+  return protocol->read_uuid_t (protocol, value, error);
+}
+
+int32_t
 thrift_protocol_skip (ThriftProtocol *protocol, ThriftType type, int *error)
 {
   switch (type)
@@ -506,6 +520,11 @@ thrift_protocol_skip (ThriftProtocol *protocol, ThriftType type, int *error)
         u_int32_t ret = thrift_protocol_read_binary (protocol, &data, &len, error);
         os_free (data);
         return ret;
+      }
+    case T_UUID:
+      {
+        uuid_t uuid;
+        return thrift_protocol_read_uuid_t (protocol, &uuid, error);
       }
     case T_STRUCT:
       {

sandesh/library/c/protocol/thrift_protocol.h
@@ -56,6 +56,7 @@ typedef enum {
   T_U32    = 20,
   T_XML    = 21,
   T_IPV4   = 22,
+  T_UUID   = 23,
 } ThriftType;
 
 /**
@@ -137,6 +138,8 @@ struct _ThriftProtocol
                           const u_int32_t len, int *error);
   int32_t (*write_xml) (struct _ThriftProtocol *protocol, const char *str,
                         int *error);
+  int32_t (*write_uuid_t) (struct _ThriftProtocol *protocol, const uuid_t value,
+                        int *error);
 
   int32_t (*read_sandesh_begin) (struct _ThriftProtocol *protocol, char **name,
                                  int *error);
@@ -178,6 +181,7 @@ struct _ThriftProtocol
   int32_t (*read_binary) (struct _ThriftProtocol *protocol, void **buf,
                           u_int32_t *len, int *error);
   int32_t (*read_xml) (struct _ThriftProtocol *protocol, char **str, int *error);
+  int32_t (*read_uuid_t) (struct _ThriftProtocol *protocol, uuid_t *value, int *error);
 };
 typedef struct _ThriftProtocol ThriftProtocol;
 
@@ -281,6 +285,9 @@ int32_t thrift_protocol_write_binary (ThriftProtocol *protocol,
 int32_t thrift_protocol_write_xml (ThriftProtocol *protocol,
                                    const char *str, int *error);
 
+int32_t thrift_protocol_write_uuid_t (ThriftProtocol *protocol,
+                                   const uuid_t value, int *error);
+
 int32_t thrift_protocol_read_message_begin (ThriftProtocol *thrift_protocol,
                                             char **name,
                                             ThriftMessageType *message_type,
@@ -370,6 +377,9 @@ int32_t thrift_protocol_read_binary (ThriftProtocol *protocol,
 
 int32_t thrift_protocol_read_xml (ThriftProtocol *protocol,
                                   char **str, int *error);
+
+int32_t thrift_protocol_read_uuid_t (ThriftProtocol *protocol,
+                                  uuid_t *value, int *error);
 
 int32_t thrift_protocol_skip (ThriftProtocol *protocol, ThriftType type,
                               int *error);

sandesh/library/cpp/protocol/TXMLProtocol.cpp
@@ -62,6 +62,7 @@ static const std::string kTypeNameDouble("double");
 static const std::string kTypeNameStruct("struct");
 static const std::string kTypeNameString("string");
 static const std::string kTypeNameXML("xml");
+static const std::string kTypeNameUUID("uuid_t");
 static const std::string kTypeNameMap("map");
 static const std::string kTypeNameList("list");
 static const std::string kTypeNameSet("set");
@@ -86,7 +87,7 @@ const std::string& TXMLProtocol::fieldTypeName(TType type) {
     case T_U16    : return kTypeNameU16    ;
     case T_U32    : return kTypeNameU32    ;
     case T_U64    : return kTypeNameU64    ;
-    case T_IPV4   : return kTypeNameIPV4    ;
+    case T_IPV4   : return kTypeNameIPV4   ;
     case T_DOUBLE : return kTypeNameDouble ;
     case T_STRING : return kTypeNameString ;
     case T_STRUCT : return kTypeNameStruct ;
@@ -95,6 +96,7 @@ const std::string& TXMLProtocol::fieldTypeName(TType type) {
     case T_LIST   : return kTypeNameList   ;
     case T_SANDESH: return kTypeNameSandesh;
     case T_XML    : return kTypeNameXML    ;
+    case T_UUID   : return kTypeNameUUID   ;
     default: return kTypeNameUnknown;
   }
 }
@@ -168,6 +170,9 @@ TType TXMLProtocol::getTypeIDForTypeName(const std::string &name) {
           break;
         case '6':
           result = T_U64;
+          break;
+        case 'u':
+          result = T_UUID;
           break;
       }
       break;
@@ -641,6 +646,10 @@ int32_t TXMLProtocol::writeXML(const string& str) {
   return writePlain(xmlstr);
 }
 
+int32_t TXMLProtocol::writeUUID(const boost::uuids::uuid& uuid) {
+  const std::string str = boost::uuids::to_string(uuid);
+  return writeString(str);
+}
 /**
  * Reading functions
  */
@@ -1137,6 +1146,18 @@ int32_t TXMLProtocol::readBinary(std::string &str) {
 
 int32_t TXMLProtocol::readXML(std::string &str) {
   return readXMLCDATA(str);
+}
+
+int32_t TXMLProtocol::readUUID(boost::uuids::uuid &uuid) {
+  int32_t ret;
+  std::string str;
+  if ((ret = readXMLString(str)) < 0) {
+    return ret;
+  }
+  std::stringstream ss;
+  ss << str;
+  ss >> uuid;
+  return ret;
 }
 
 }}} // contrail::sandesh::protocol

sandesh/library/python/pysandesh/protocol/TXMLProtocol.py
@@ -4,6 +4,7 @@
 
 import ctypes
 import re
+import uuid
 from TProtocol import *
 from  pysandesh.sandesh_logger import SandeshLogger
 
@@ -35,6 +36,7 @@ class TXMLProtocol(TProtocolBase):
   _XML_TYPENAME_U64 = 'u64'
   _XML_TYPENAME_IPV4 = 'ipv4'
   _XML_TYPENAME_DOUBLE = 'double'
+  _XML_TYPENAME_UUID = 'uuid_t'
   _XML_TYPENAME_STRING = 'string'
   _XML_TYPENAME_XML = 'xml'
   _XML_TYPENAME_STRUCT = 'struct'
@@ -66,6 +68,7 @@ class TXMLProtocol(TProtocolBase):
       TType.LIST : self._XML_TYPENAME_LIST,
       TType.SANDESH : self._XML_TYPENAME_SANDESH,
       TType.XML : self._XML_TYPENAME_XML,
+      TType.UUID : self._XML_TYPENAME_UUID,
     }
 
     self._field_type_dict = {}
@@ -292,6 +295,14 @@ class TXMLProtocol(TProtocolBase):
     self.writeBuffer(self._XML_CDATA_OPEN)
     self.writeBuffer(xml)
     self.writeBuffer(self._XML_CDATA_CLOSE)
+    return 0
+
+  def writeUUID(self, uuid):
+    try:
+      self.writeBuffer(str(uuid))
+    except TypeError:
+      self._logger.error('TXML Protocol: Invalid uuid_t value %s' % str(uuid))
+      return -1
     return 0
     
   class XMLReader:
@@ -803,6 +814,17 @@ class TXMLProtocol(TProtocolBase):
     if xml is None:
       return (-1, None)
     return (len(xml), xml)      
+
+  def readUUID(self):
+    (uuid_str, length) = self._xml_reader.readXMLValue()
+    if uuid_str is None:
+      return (-1, None)
+    try:
+      uuid_temp = uuid.UUID(uuid_str)
+    except ValueError:
+      self._logger.error('TXML Protocol: Invalid uuid_t value "%s".' %(uuid_str))
+      return (-1, None)
+    return (length, uuid_temp)
 
 class TXMLProtocolFactory:
   def __init__(self, strictRead=False, strictWrite=True):