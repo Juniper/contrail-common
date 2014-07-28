Juniper/contrail-sandesh

Adding support for new type uuid in sandesh
Change-Id: I2306fde9eea9c3188ea66c0863dd09162ac0822a

compiler/generate/t_c_generator.cc
@@ -568,6 +568,8 @@ string t_c_generator::base_type_name(t_base_type *type) {
       return "double";
     case t_base_type::TYPE_IPV4:
       return "uint32_t";
+    case t_base_type::TYPE_UUID:
+      return "uuid_t";
     default:
       throw "compiler error: no C base type name for base type "
             + t_base_type::t_base_name (tbase);
@@ -621,6 +623,8 @@ string t_c_generator::type_to_enum (t_type *type) {
         return "T_DOUBLE";
       case t_base_type::TYPE_IPV4:
         return "T_IPV4";
+      case t_base_type::TYPE_UUID:
+        return "T_UUID";
     }
   } else if (type->is_enum()) {
     return "T_I32";
@@ -666,6 +670,9 @@ string t_c_generator::constant_value(string name, t_type *type, t_const_value *v
       case t_base_type::TYPE_U64:
       case t_base_type::TYPE_IPV4:
         render << value->get_integer();
+        break;
+      case t_base_type::TYPE_UUID:
+        render << value->get_uuid(); // Need to fix later, c structure is not getting initialize.
         break;
       case t_base_type::TYPE_DOUBLE:
         if (value->get_type() == t_const_value::CV_INTEGER) {
@@ -809,6 +816,9 @@ string t_c_generator::declare_field(t_field *tfield,
         case t_base_type::TYPE_STRING:
         case t_base_type::TYPE_XML:
           result += " = NULL";
+          break;
+        case t_base_type::TYPE_UUID:
+          result += "{0}";
           break;
         default:
           throw "compiler error: no C intializer for base type "
@@ -1404,6 +1414,9 @@ void t_c_generator::generate_serialize_field(ofstream &out,
         case t_base_type::TYPE_XML:
           out << "xml (protocol, " << name;
           break;
+        case t_base_type::TYPE_UUID:
+          out << "uuid (protocol, " << name;
+          break;
         default:
           throw "compiler error: no C writer for base type "
                 + t_base_type::t_base_name (tbase) + name;
@@ -1560,6 +1573,9 @@ void t_c_generator::generate_deserialize_field(ofstream &out,
         break;
       case t_base_type::TYPE_IPV4:
         out << "ipv4 (protocol, &" << name;
+        break;
+      case t_base_type::TYPE_UUID:
+        out << "uuid (protocol, &" << name;
         break;
       default:
         throw "compiler error: no C reader for base type "

compiler/globals.h
@@ -86,6 +86,8 @@ extern t_type* g_type_u32;
 extern t_type* g_type_u64;
 extern t_type* g_type_ipv4;
 extern t_type* g_type_xml;
+extern t_type* g_type_uuid;
+
 #endif
 extern t_type* g_type_binary;
 extern t_type* g_type_slist;

compiler/main.cc
@@ -92,6 +92,8 @@ t_type* g_type_sandesh_uve;
 t_type* g_type_sandesh_object;
 t_type* g_type_sandesh_flow;
 t_type* g_type_xml;
+t_type* g_type_uuid;
+
 #endif
 
 
@@ -780,6 +782,12 @@ void validate_const_rec(std::string name, t_type* type, t_const_value* value) {
       if (value->get_type() != t_const_value::CV_INTEGER) {
         throw "type error: const \"" + name + "\" was declared as u64";
       }
+      break;
+    case t_base_type::TYPE_UUID:
+      if (value->get_type() != t_const_value::CV_STRING) {
+        throw "type error: const \"" + name + "\" was declared as uuid";
+      }
+      break;
 #endif
     case t_base_type::TYPE_BOOL:
       if (value->get_type() != t_const_value::CV_INTEGER) {
@@ -1332,6 +1340,7 @@ int main(int argc, char** argv) {
   g_type_u64    = new t_base_type("u64", t_base_type::TYPE_U64);
   g_type_ipv4    = new t_base_type("ipv4", t_base_type::TYPE_IPV4);
   g_type_xml    = new t_base_type("xml", t_base_type::TYPE_XML);
+  g_type_uuid    = new t_base_type("uuid", t_base_type::TYPE_UUID);
   g_type_static_const_string = new t_base_type("static const string", t_base_type::TYPE_STATIC_CONST_STRING);
   g_type_sandesh_system   = new t_base_type("system",   t_base_type::TYPE_SANDESH_SYSTEM);
   g_type_sandesh_request  = new t_base_type("request",  t_base_type::TYPE_SANDESH_REQUEST);
@@ -1385,6 +1394,8 @@ int main(int argc, char** argv) {
   delete g_type_sandesh_object;
   delete g_type_sandesh_flow;
   delete g_type_xml;
+  delete g_type_uuid;
+
 #endif
 
   // Finished

compiler/parse/t_base_type.h
@@ -59,6 +59,7 @@ class t_base_type : public t_type {
     TYPE_U64,
     TYPE_XML,
     TYPE_IPV4,
+    TYPE_UUID,
 #endif
     TYPE_DOUBLE
   };
@@ -87,6 +88,9 @@ class t_base_type : public t_type {
   }
 
 #ifdef SANDESH
+  bool is_uuid() const {
+    return base_ == TYPE_UUID;
+  }
   bool is_static_const_string() const {
     return base_ == TYPE_STATIC_CONST_STRING;
   }
@@ -203,6 +207,7 @@ class t_base_type : public t_type {
       case TYPE_U64    : return       "u64"; break;
       case TYPE_IPV4   : return       "ipv4"; break;
       case TYPE_XML    : return       "xml"; break;
+      case TYPE_UUID   : return       "uuid"; break;
       case TYPE_STATIC_CONST_STRING :
           return    "static const string"; break;
       case TYPE_SANDESH_SYSTEM   : return      "system"; break;

Juniper/contrail-common

sandesh/compiler/generate/t_c_generator.cc
@@ -568,6 +568,8 @@ string t_c_generator::base_type_name(t_base_type *type) {
       return "double";
     case t_base_type::TYPE_IPV4:
       return "uint32_t";
+    case t_base_type::TYPE_UUID:
+      return "uuid_t";
     default:
       throw "compiler error: no C base type name for base type "
             + t_base_type::t_base_name (tbase);
@@ -621,6 +623,8 @@ string t_c_generator::type_to_enum (t_type *type) {
         return "T_DOUBLE";
       case t_base_type::TYPE_IPV4:
         return "T_IPV4";
+      case t_base_type::TYPE_UUID:
+        return "T_UUID";
     }
   } else if (type->is_enum()) {
     return "T_I32";
@@ -666,6 +670,9 @@ string t_c_generator::constant_value(string name, t_type *type, t_const_value *v
       case t_base_type::TYPE_U64:
       case t_base_type::TYPE_IPV4:
         render << value->get_integer();
+        break;
+      case t_base_type::TYPE_UUID:
+        render << value->get_uuid(); // Need to fix later, c structure is not getting initialize.
         break;
       case t_base_type::TYPE_DOUBLE:
         if (value->get_type() == t_const_value::CV_INTEGER) {
@@ -809,6 +816,9 @@ string t_c_generator::declare_field(t_field *tfield,
         case t_base_type::TYPE_STRING:
         case t_base_type::TYPE_XML:
           result += " = NULL";
+          break;
+        case t_base_type::TYPE_UUID:
+          result += "{0}";
           break;
         default:
           throw "compiler error: no C intializer for base type "
@@ -1404,6 +1414,9 @@ void t_c_generator::generate_serialize_field(ofstream &out,
         case t_base_type::TYPE_XML:
           out << "xml (protocol, " << name;
           break;
+        case t_base_type::TYPE_UUID:
+          out << "uuid_t (protocol, " << name;
+          break;
         default:
           throw "compiler error: no C writer for base type "
                 + t_base_type::t_base_name (tbase) + name;
@@ -1560,6 +1573,9 @@ void t_c_generator::generate_deserialize_field(ofstream &out,
         break;
       case t_base_type::TYPE_IPV4:
         out << "ipv4 (protocol, &" << name;
+        break;
+      case t_base_type::TYPE_UUID:
+        out << "uuid_t (protocol, &" << name;
         break;
       default:
         throw "compiler error: no C reader for base type "

sandesh/compiler/globals.h
@@ -86,6 +86,8 @@ extern t_type* g_type_u32;
 extern t_type* g_type_u64;
 extern t_type* g_type_ipv4;
 extern t_type* g_type_xml;
+extern t_type* g_type_uuid_t;
+
 #endif
 extern t_type* g_type_binary;
 extern t_type* g_type_slist;

sandesh/compiler/main.cc
@@ -92,6 +92,8 @@ t_type* g_type_sandesh_uve;
 t_type* g_type_sandesh_object;
 t_type* g_type_sandesh_flow;
 t_type* g_type_xml;
+t_type* g_type_uuid_t;
+
 #endif
 
 
@@ -780,6 +782,12 @@ void validate_const_rec(std::string name, t_type* type, t_const_value* value) {
       if (value->get_type() != t_const_value::CV_INTEGER) {
         throw "type error: const \"" + name + "\" was declared as u64";
       }
+      break;
+    case t_base_type::TYPE_UUID:
+      if (value->get_type() != t_const_value::CV_STRING) {
+        throw "type error: const \"" + name + "\" was declared as uuid_t";
+      }
+      break;
 #endif
     case t_base_type::TYPE_BOOL:
       if (value->get_type() != t_const_value::CV_INTEGER) {
@@ -1332,6 +1340,7 @@ int main(int argc, char** argv) {
   g_type_u64    = new t_base_type("u64", t_base_type::TYPE_U64);
   g_type_ipv4    = new t_base_type("ipv4", t_base_type::TYPE_IPV4);
   g_type_xml    = new t_base_type("xml", t_base_type::TYPE_XML);
+  g_type_uuid_t    = new t_base_type("uuid_t", t_base_type::TYPE_UUID);
   g_type_static_const_string = new t_base_type("static const string", t_base_type::TYPE_STATIC_CONST_STRING);
   g_type_sandesh_system   = new t_base_type("system",   t_base_type::TYPE_SANDESH_SYSTEM);
   g_type_sandesh_request  = new t_base_type("request",  t_base_type::TYPE_SANDESH_REQUEST);
@@ -1385,6 +1394,8 @@ int main(int argc, char** argv) {
   delete g_type_sandesh_object;
   delete g_type_sandesh_flow;
   delete g_type_xml;
+  delete g_type_uuid_t;
+
 #endif
 
   // Finished

sandesh/compiler/parse/t_base_type.h
@@ -59,6 +59,7 @@ class t_base_type : public t_type {
     TYPE_U64,
     TYPE_XML,
     TYPE_IPV4,
+    TYPE_UUID,
 #endif
     TYPE_DOUBLE
   };
@@ -87,6 +88,9 @@ class t_base_type : public t_type {
   }
 
 #ifdef SANDESH
+  bool is_uuid() const {
+    return base_ == TYPE_UUID;
+  }
   bool is_static_const_string() const {
     return base_ == TYPE_STATIC_CONST_STRING;
   }
@@ -203,6 +207,7 @@ class t_base_type : public t_type {
       case TYPE_U64    : return       "u64"; break;
       case TYPE_IPV4   : return       "ipv4"; break;
       case TYPE_XML    : return       "xml"; break;
+      case TYPE_UUID   : return       "uuid_t"; break;
       case TYPE_STATIC_CONST_STRING :
           return    "static const string"; break;
       case TYPE_SANDESH_SYSTEM   : return      "system"; break;