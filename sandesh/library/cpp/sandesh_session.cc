Juniper/contrail-sandesh

TcpSession::Send() and TcpSession::WriteReady() can be called from concurrent threads and hence just having an atomic bool to update the send status is not enough to guarantee that the correct send status is finally set. A recent change in SandeshWriter removed the mutex acquired before called TcpSession::Send() and updating the send status and used to update the send status in the TcpSession::WriteReady() callback. This change reverts back to using the mutex in SandeshWriter. Closes-Bug: #1388221
Change-Id: Ibb560021d6a0d8963db6284f1d722a974cb45be2

library/cpp/sandesh_session.cc
@@ -42,9 +42,9 @@ const std::string SandeshWriter::sandesh_close_ = sXML_SANDESH_CLOSE;
 //
 SandeshWriter::SandeshWriter(SandeshSession *session)
     : session_(session),
+    ready_to_send_(true),
     send_buf_(new uint8_t[kDefaultSendSize]),
     send_buf_offset_(0) {
-    ready_to_send_ = true;
 }
 
 SandeshWriter::~SandeshWriter() {
@@ -60,7 +60,10 @@ void SandeshWriter::WriteReady(const boost::system::error_code &ec) {
         return;
     }
 
-    ready_to_send_ = true;
+    {
+        tbb::mutex::scoped_lock lock(mutex_);
+        ready_to_send_ = true;
+    }
 
     // We may want to start the Runner for the send_queue
     session_->send_queue()->MayBeStartRunner();
@@ -265,6 +268,7 @@ void SandeshWriter::SendInternal(boost::shared_ptr<TMemoryBuffer> buf) {
     uint8_t  *buffer;
     uint32_t len;
     buf->getBuffer(&buffer, &len);
+    tbb::mutex::scoped_lock lock(mutex_);
     ready_to_send_ = session_->Send((const uint8_t *)buffer, len, NULL);
 }
 

library/cpp/sandesh_session.h
@@ -42,6 +42,7 @@ public:
     }
     void WriteReady(const boost::system::error_code &ec);
     bool SendReady() {
+        tbb::mutex::scoped_lock lock(mutex_);
         return ready_to_send_;
     }
 
@@ -79,7 +80,8 @@ private:
         send_buf_offset_ = 0;
     }
 
-    tbb::atomic<bool> ready_to_send_;
+    tbb::mutex mutex_;
+    bool ready_to_send_;
     // send_buf_ is used to store unsent data
     uint8_t *send_buf_;
     size_t send_buf_offset_;

Juniper/contrail-common

sandesh/library/cpp/sandesh_session.cc
@@ -42,9 +42,9 @@ const std::string SandeshWriter::sandesh_close_ = sXML_SANDESH_CLOSE;
 //
 SandeshWriter::SandeshWriter(SandeshSession *session)
     : session_(session),
+    ready_to_send_(true),
     send_buf_(new uint8_t[kDefaultSendSize]),
     send_buf_offset_(0) {
-    ready_to_send_ = true;
 }
 
 SandeshWriter::~SandeshWriter() {
@@ -60,7 +60,10 @@ void SandeshWriter::WriteReady(const boost::system::error_code &ec) {
         return;
     }
 
-    ready_to_send_ = true;
+    {
+        tbb::mutex::scoped_lock lock(send_mutex_);
+        ready_to_send_ = true;
+    }
 
     // We may want to start the Runner for the send_queue
     session_->send_queue()->MayBeStartRunner();
@@ -265,6 +268,7 @@ void SandeshWriter::SendInternal(boost::shared_ptr<TMemoryBuffer> buf) {
     uint8_t  *buffer;
     uint32_t len;
     buf->getBuffer(&buffer, &len);
+    tbb::mutex::scoped_lock lock(send_mutex_);
     ready_to_send_ = session_->Send((const uint8_t *)buffer, len, NULL);
 }
 

sandesh/library/cpp/sandesh_session.h
@@ -42,6 +42,7 @@ public:
     }
     void WriteReady(const boost::system::error_code &ec);
     bool SendReady() {
+        tbb::mutex::scoped_lock lock(send_mutex_);
         return ready_to_send_;
     }
 
@@ -79,7 +80,8 @@ private:
         send_buf_offset_ = 0;
     }
 
-    tbb::atomic<bool> ready_to_send_;
+    tbb::mutex send_mutex_;
+    bool ready_to_send_;
     // send_buf_ is used to store unsent data
     uint8_t *send_buf_;
     size_t send_buf_offset_;