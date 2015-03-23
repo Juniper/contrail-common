Juniper/contrail-sandesh

Support logging customization
Presently some of the logging parameters are hard coded.

This patch provides the option to customize the
logging parameters in Open Contrail.

With this patch the user can choose to
 - Use the existing logging mechanism or
 - Completely override the logging parameters by
    - using SandeshConfigLogger class and defining a log file based on
      https://docs.python.org/2/library/logging.config.html#module-logging.config)
    - Or can define new logging mechanism by implementing his own logger.

Also addressed few pep8 errors

Change-Id: I633e26deca634ad83cdf503409ec1331b1984b34
Partially-Implements: bp support-log-configuration

library/python/pysandesh/sandesh_logger.py
@@ -7,74 +7,74 @@
 #
 
 import logging
+import logging.config
 import logging.handlers
-from gen_py.sandesh.ttypes import SandeshLevel
 
-class SandeshLogger(object):
+from cfgm_common import importutils
+from gen_py.sandesh.ttypes import SandeshLevel
+import sandesh_base_logger
+
+
+def create_logger(generator, logger_class, logger_config_file=None):
+    l_class = importutils.import_class(logger_class)
+    return l_class(generator, logger_config_file=logger_config_file)
+
+
+class SandeshConfigLogger(sandesh_base_logger.SandeshBaseLogger):
+
+    """Sandesh Config Logger Implementation.
+
+    This class sets the log config file to the python logging module.
+    The user should define the log config file as per format defined in [1].
+
+    [1] https://docs.python.org/2/library/logging.config.html
+    """
+
+    def __init__(self, generator, logger_config_file=None):
+        super(SandeshConfigLogger, self).__init__(generator)
+        logging.config.fileConfig(logger_config_file)
+        self._logger = logging.getLogger(generator)
+
+
+class SandeshLogger(sandesh_base_logger.SandeshBaseLogger):
 
     """Sandesh Logger Implementation."""
-
-    _logger = None
     _DEFAULT_LOG_FILE = '<stdout>'
     _DEFAULT_SYSLOG_FACILITY = 'LOG_LOCAL0'
 
-    _SANDESH_LEVEL_TO_LOGGER_LEVEL = {
-        SandeshLevel.SYS_EMERG : logging.CRITICAL,
-        SandeshLevel.SYS_ALERT : logging.CRITICAL,
-        SandeshLevel.SYS_CRIT : logging.CRITICAL,
-        SandeshLevel.SYS_ERR : logging.ERROR,
-        SandeshLevel.SYS_WARN : logging.WARNING,
-        SandeshLevel.SYS_NOTICE : logging.WARNING,
-        SandeshLevel.SYS_INFO : logging.INFO,
-        SandeshLevel.SYS_DEBUG : logging.DEBUG
-    }
-
-    def __init__(self, generator):
+    def __init__(self, generator, logger_config_file=None):
         assert generator, 'SandeshLogger init requires generator name'
 
+        super(SandeshLogger, self).__init__(generator)
+
         self._generator = generator
-        self._enable_local_log = False
-        self._enable_syslog = False
-        self._logging_category = ''
-        self._logging_file = self._DEFAULT_LOG_FILE
-        self._logging_syslog_facility = self._DEFAULT_SYSLOG_FACILITY
+
         self._logger = logging.getLogger(self._generator)
-        self._logging_level = SandeshLevel.SYS_INFO
-        self._logger.setLevel(SandeshLogger.get_py_logger_level(self._logging_level))
+        self._logger.setLevel(
+            sandesh_base_logger.SandeshBaseLogger.get_py_logger_level(
+                SandeshLevel.SYS_INFO))
         if not len(self._logger.handlers):
             # add the handler only once
             self._logging_file_handler = logging.StreamHandler()
-            log_format = logging.Formatter('%(asctime)s [%(name)s]: %(message)s',
-                                           datefmt='%m/%d/%Y %I:%M:%S %p')
+            log_format = logging.Formatter(
+                '%(asctime)s [%(name)s]: %(message)s',
+                datefmt='%m/%d/%Y %I:%M:%S %p')
             self._logging_file_handler.setFormatter(log_format)
             self._logger.addHandler(self._logging_file_handler)
         else:
             self._logging_file_handler = self._logger.handlers[0]
-    #end __init__
 
-    @staticmethod
-    def get_py_logger_level(sandesh_level):
-        return SandeshLogger._SANDESH_LEVEL_TO_LOGGER_LEVEL[sandesh_level]
-    #end get_py_logger_level
+    # end __init__
 
-    def logger(self):
-        return self._logger
-    #end logger
-
-    def set_logging_params(self, enable_local_log=False, category='', level=SandeshLevel.SYS_INFO,
-                           file=_DEFAULT_LOG_FILE, enable_syslog=False, syslog_facility='LOG_LOCAL0'):
+    def set_logging_params(self, enable_local_log=False, category='',
+                           level=SandeshLevel.SYS_INFO, file=_DEFAULT_LOG_FILE,
+                           enable_syslog=False, syslog_facility='LOG_LOCAL0'):
         self.set_local_logging(enable_local_log)
         self.set_logging_category(category)
         self.set_logging_level(level)
         self.set_logging_file(file)
         self.set_logging_syslog(enable_syslog, syslog_facility)
-    #end set_logging_params
-    
-    def set_local_logging(self, enable_local_log):
-        if self._enable_local_log != enable_local_log:
-            self._logger.info('SANDESH: Logging: %s -> %s', self._enable_local_log, enable_local_log)
-            self._enable_local_log = enable_local_log
-    #end set_local_logging
+    # end set_logging_params
 
     def set_logging_level(self, level):
         if isinstance(level, str):
@@ -89,84 +89,54 @@ class SandeshLogger(object):
             logger_level = logging.INFO
             level = SandeshLevel.SYS_INFO
 
-        if self._logging_level != level:
-            self._logger.info('SANDESH: Logging: LEVEL: [%s] -> [%s]', 
-                              SandeshLevel._VALUES_TO_NAMES[self._logging_level],
-                              SandeshLevel._VALUES_TO_NAMES[level])
-            self._logging_level = level
-            self._logger.setLevel(logger_level)
-    #end set_logging_level
-    
-    def set_logging_category(self, category):
-        if self._logging_category != category:
-            self._logger.info('SANDESH: Logging: CATEGORY: %s -> %s', self._logging_category, category)
-            self._logging_category = category
-    #end set_logging_category
-    
+        self._logger.info('SANDESH: Logging: LEVEL: [%s] -> [%s]',
+                          SandeshLevel._VALUES_TO_NAMES[self.logging_level()],
+                          SandeshLevel._VALUES_TO_NAMES[level])
+        self._logger.setLevel(logger_level)
+        super(SandeshLogger, self).set_logging_level(level)
+    # end set_logging_level
+
     def set_logging_file(self, file):
-        if self._logging_file != file:
+        if self.logging_file() != file:
             self._logger.info('SANDESH: Logging: FILE: [%s] -> [%s]',
-                              self._logging_file, file)
+                              self.logging_file(), file)
             self._logger.removeHandler(self._logging_file_handler)
             if file == self._DEFAULT_LOG_FILE:
                 self._logging_file_handler = logging.StreamHandler()
             else:
-                self._logging_file_handler = logging.handlers.RotatingFileHandler(filename=file,
-                                                                                  maxBytes=5000000,
-                                                                                  backupCount=10)
-            log_format = logging.Formatter('%(asctime)s [%(name)s]: %(message)s',
-                                           datefmt='%m/%d/%Y %I:%M:%S %p')
+                self._logging_file_handler = (
+                    logging.handlers.RotatingFileHandler(
+                        filename=file, maxBytes=5000000, backupCount=10))
+            log_format = logging.Formatter(
+                '%(asctime)s [%(name)s]: %(message)s',
+                datefmt='%m/%d/%Y %I:%M:%S %p')
             self._logging_file_handler.setFormatter(log_format)
             self._logger.addHandler(self._logging_file_handler)
-            self._logging_file = file   
-    #end set_logging_file        
+            super(SandeshLogger, self).set_logging_file(file)
+    # end set_logging_file
 
     def set_logging_syslog(self, enable_syslog, syslog_facility):
-        if self._enable_syslog == enable_syslog and \
-           self._logging_syslog_facility == syslog_facility:
+        if (self.is_syslog_logging_enabled() == enable_syslog and
+           self.logging_syslog_facility() == syslog_facility):
             return
 
-        if self._logging_syslog_facility != syslog_facility:
+        if self.logging_syslog_facility() != syslog_facility:
             self._logger.info('SANDESH: Logging: SYSLOG: [%s] -> [%s]',
-                              self._logging_syslog_facility, syslog_facility)
+                              self.logging_syslog_facility(), syslog_facility)
 
-        if self._enable_syslog:
+        if self.is_syslog_logging_enabled():
             self._logger.removeHandler(self._logging_syslog_handler)
 
         if enable_syslog:
             self._logging_syslog_handler = logging.handlers.SysLogHandler(
                 address="/dev/log",
-                facility=getattr(logging.handlers.SysLogHandler, syslog_facility,
+                facility=getattr(logging.handlers.SysLogHandler,
+                                 syslog_facility,
                                  logging.handlers.SysLogHandler.LOG_LOCAL0)
             )
             self._logger.addHandler(self._logging_syslog_handler)
 
-        self._enable_syslog = enable_syslog
-        self._logging_syslog_facility = syslog_facility
-    #end set_logging_syslog
-
-    def is_local_logging_enabled(self):
-        return self._enable_local_log
-    #end is_local_logging_enabled
-    
-    def logging_level(self):
-        return self._logging_level
-    #end logging_level
-    
-    def logging_category(self):
-        return self._logging_category
-    #end logging_category
-    
-    def logging_file(self):
-        return self._logging_file
-    #end logging_file
-
-    def is_syslog_logging_enabled(self):
-        return self._enable_syslog_log
-    #end is_syslog_logging_enabled
-
-    def logging_syslog_facility(self):
-        return self._logging_syslog_facility
-    #end logging_syslog_facility
-
-#end class SandeshLogger
+        super(SandeshLogger, self).set_logging_syslog(enable_syslog,
+                                                      syslog_facility)
+    # end set_logging_syslog
+# end class SandeshLogger

Juniper/contrail-common

sandesh/library/python/pysandesh/sandesh_logger.py
@@ -7,74 +7,75 @@
 #
 
 import logging
+import logging.config
 import logging.handlers
+
 from gen_py.sandesh.ttypes import SandeshLevel
 
-class SandeshLogger(object):
+import sandesh_base_logger
+import util
+
+
+def create_logger(generator, logger_class, logger_config_file=None):
+    l_class = util.import_class(logger_class)
+    return l_class(generator, logger_config_file=logger_config_file)
+
+
+class SandeshConfigLogger(sandesh_base_logger.SandeshBaseLogger):
+
+    """Sandesh Config Logger Implementation.
+
+    This class sets the log config file to the python logging module.
+    The user should define the log config file as per format defined in [1].
+
+    [1] https://docs.python.org/2/library/logging.config.html
+    """
+
+    def __init__(self, generator, logger_config_file=None):
+        super(SandeshConfigLogger, self).__init__(generator)
+        logging.config.fileConfig(logger_config_file)
+        self._logger = logging.getLogger(generator)
+
+
+class SandeshLogger(sandesh_base_logger.SandeshBaseLogger):
 
     """Sandesh Logger Implementation."""
-
-    _logger = None
     _DEFAULT_LOG_FILE = '<stdout>'
     _DEFAULT_SYSLOG_FACILITY = 'LOG_LOCAL0'
 
-    _SANDESH_LEVEL_TO_LOGGER_LEVEL = {
-        SandeshLevel.SYS_EMERG : logging.CRITICAL,
-        SandeshLevel.SYS_ALERT : logging.CRITICAL,
-        SandeshLevel.SYS_CRIT : logging.CRITICAL,
-        SandeshLevel.SYS_ERR : logging.ERROR,
-        SandeshLevel.SYS_WARN : logging.WARNING,
-        SandeshLevel.SYS_NOTICE : logging.WARNING,
-        SandeshLevel.SYS_INFO : logging.INFO,
-        SandeshLevel.SYS_DEBUG : logging.DEBUG
-    }
-
-    def __init__(self, generator):
+    def __init__(self, generator, logger_config_file=None):
         assert generator, 'SandeshLogger init requires generator name'
 
+        super(SandeshLogger, self).__init__(generator)
+
         self._generator = generator
-        self._enable_local_log = False
-        self._enable_syslog = False
-        self._logging_category = ''
-        self._logging_file = self._DEFAULT_LOG_FILE
-        self._logging_syslog_facility = self._DEFAULT_SYSLOG_FACILITY
+
         self._logger = logging.getLogger(self._generator)
-        self._logging_level = SandeshLevel.SYS_INFO
-        self._logger.setLevel(SandeshLogger.get_py_logger_level(self._logging_level))
+        self._logger.setLevel(
+            sandesh_base_logger.SandeshBaseLogger.get_py_logger_level(
+                SandeshLevel.SYS_INFO))
         if not len(self._logger.handlers):
             # add the handler only once
             self._logging_file_handler = logging.StreamHandler()
-            log_format = logging.Formatter('%(asctime)s [%(name)s]: %(message)s',
-                                           datefmt='%m/%d/%Y %I:%M:%S %p')
+            log_format = logging.Formatter(
+                '%(asctime)s [%(name)s]: %(message)s',
+                datefmt='%m/%d/%Y %I:%M:%S %p')
             self._logging_file_handler.setFormatter(log_format)
             self._logger.addHandler(self._logging_file_handler)
         else:
             self._logging_file_handler = self._logger.handlers[0]
-    #end __init__
 
-    @staticmethod
-    def get_py_logger_level(sandesh_level):
-        return SandeshLogger._SANDESH_LEVEL_TO_LOGGER_LEVEL[sandesh_level]
-    #end get_py_logger_level
+    # end __init__
 
-    def logger(self):
-        return self._logger
-    #end logger
-
-    def set_logging_params(self, enable_local_log=False, category='', level=SandeshLevel.SYS_INFO,
-                           file=_DEFAULT_LOG_FILE, enable_syslog=False, syslog_facility='LOG_LOCAL0'):
+    def set_logging_params(self, enable_local_log=False, category='',
+                           level=SandeshLevel.SYS_INFO, file=_DEFAULT_LOG_FILE,
+                           enable_syslog=False, syslog_facility='LOG_LOCAL0'):
         self.set_local_logging(enable_local_log)
         self.set_logging_category(category)
         self.set_logging_level(level)
         self.set_logging_file(file)
         self.set_logging_syslog(enable_syslog, syslog_facility)
-    #end set_logging_params
-    
-    def set_local_logging(self, enable_local_log):
-        if self._enable_local_log != enable_local_log:
-            self._logger.info('SANDESH: Logging: %s -> %s', self._enable_local_log, enable_local_log)
-            self._enable_local_log = enable_local_log
-    #end set_local_logging
+    # end set_logging_params
 
     def set_logging_level(self, level):
         if isinstance(level, str):
@@ -89,84 +90,54 @@ class SandeshLogger(object):
             logger_level = logging.INFO
             level = SandeshLevel.SYS_INFO
 
-        if self._logging_level != level:
-            self._logger.info('SANDESH: Logging: LEVEL: [%s] -> [%s]', 
-                              SandeshLevel._VALUES_TO_NAMES[self._logging_level],
-                              SandeshLevel._VALUES_TO_NAMES[level])
-            self._logging_level = level
-            self._logger.setLevel(logger_level)
-    #end set_logging_level
-    
-    def set_logging_category(self, category):
-        if self._logging_category != category:
-            self._logger.info('SANDESH: Logging: CATEGORY: %s -> %s', self._logging_category, category)
-            self._logging_category = category
-    #end set_logging_category
-    
+        self._logger.info('SANDESH: Logging: LEVEL: [%s] -> [%s]',
+                          SandeshLevel._VALUES_TO_NAMES[self.logging_level()],
+                          SandeshLevel._VALUES_TO_NAMES[level])
+        self._logger.setLevel(logger_level)
+        super(SandeshLogger, self).set_logging_level(level)
+    # end set_logging_level
+
     def set_logging_file(self, file):
-        if self._logging_file != file:
+        if self.logging_file() != file:
             self._logger.info('SANDESH: Logging: FILE: [%s] -> [%s]',
-                              self._logging_file, file)
+                              self.logging_file(), file)
             self._logger.removeHandler(self._logging_file_handler)
             if file == self._DEFAULT_LOG_FILE:
                 self._logging_file_handler = logging.StreamHandler()
             else:
-                self._logging_file_handler = logging.handlers.RotatingFileHandler(filename=file,
-                                                                                  maxBytes=5000000,
-                                                                                  backupCount=10)
-            log_format = logging.Formatter('%(asctime)s [%(name)s]: %(message)s',
-                                           datefmt='%m/%d/%Y %I:%M:%S %p')
+                self._logging_file_handler = (
+                    logging.handlers.RotatingFileHandler(
+                        filename=file, maxBytes=5000000, backupCount=10))
+            log_format = logging.Formatter(
+                '%(asctime)s [%(name)s]: %(message)s',
+                datefmt='%m/%d/%Y %I:%M:%S %p')
             self._logging_file_handler.setFormatter(log_format)
             self._logger.addHandler(self._logging_file_handler)
-            self._logging_file = file   
-    #end set_logging_file        
+            super(SandeshLogger, self).set_logging_file(file)
+    # end set_logging_file
 
     def set_logging_syslog(self, enable_syslog, syslog_facility):
-        if self._enable_syslog == enable_syslog and \
-           self._logging_syslog_facility == syslog_facility:
+        if (self.is_syslog_logging_enabled() == enable_syslog and
+           self.logging_syslog_facility() == syslog_facility):
             return
 
-        if self._logging_syslog_facility != syslog_facility:
+        if self.logging_syslog_facility() != syslog_facility:
             self._logger.info('SANDESH: Logging: SYSLOG: [%s] -> [%s]',
-                              self._logging_syslog_facility, syslog_facility)
+                              self.logging_syslog_facility(), syslog_facility)
 
-        if self._enable_syslog:
+        if self.is_syslog_logging_enabled():
             self._logger.removeHandler(self._logging_syslog_handler)
 
         if enable_syslog:
             self._logging_syslog_handler = logging.handlers.SysLogHandler(
                 address="/dev/log",
-                facility=getattr(logging.handlers.SysLogHandler, syslog_facility,
+                facility=getattr(logging.handlers.SysLogHandler,
+                                 syslog_facility,
                                  logging.handlers.SysLogHandler.LOG_LOCAL0)
             )
             self._logger.addHandler(self._logging_syslog_handler)
 
-        self._enable_syslog = enable_syslog
-        self._logging_syslog_facility = syslog_facility
-    #end set_logging_syslog
-
-    def is_local_logging_enabled(self):
-        return self._enable_local_log
-    #end is_local_logging_enabled
-    
-    def logging_level(self):
-        return self._logging_level
-    #end logging_level
-    
-    def logging_category(self):
-        return self._logging_category
-    #end logging_category
-    
-    def logging_file(self):
-        return self._logging_file
-    #end logging_file
-
-    def is_syslog_logging_enabled(self):
-        return self._enable_syslog_log
-    #end is_syslog_logging_enabled
-
-    def logging_syslog_facility(self):
-        return self._logging_syslog_facility
-    #end logging_syslog_facility
-
-#end class SandeshLogger
+        super(SandeshLogger, self).set_logging_syslog(enable_syslog,
+                                                      syslog_facility)
+    # end set_logging_syslog
+# end class SandeshLogger