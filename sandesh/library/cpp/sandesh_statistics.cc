Juniper/contrail-sandesh

This fix adds sandesh systemlog message throttling support. We are throttling the message on the send side with a circular buffer of certail value. The size of the buffer would be the messages/second that can be sent by the application. If application sends more messages than the configured rate, it will be dropped and a syslog message is recorded for each overshoot.Following logic is added in the send part for both c++ and python:
1. Record the current time in curr_time
2. If CB is full:
      2.1. Compare time of the next free buffer with current time
      2.2. If difference is more than a sec, then enque else drop
3. enqueue in CB

Currently the CB size cannot be changed midway
Partial-Bug: 1469414

Change-Id: I2817ed643f1cd02ca916332c0d4f09e3f7b5f2c5

library/cpp/sandesh_statistics.cc
@@ -42,6 +42,10 @@ static void UpdateSandeshMessageStatsDrops(SandeshMessageStats *smstats,
             smstats->messages_sent_dropped_validation_failed++;
             smstats->bytes_sent_dropped_validation_failed += bytes;
             break;
+          case SandeshTxDropReason::BufferFull:
+            smstats->messages_sent_dropped_buffer_full++;
+            smstats->bytes_sent_dropped_buffer_full += bytes;
+            break;
           case SandeshTxDropReason::QueueLevel:
             smstats->messages_sent_dropped_queue_level++;
             smstats->bytes_sent_dropped_queue_level += bytes;

library/python/pysandesh/sandesh_stats.py
@@ -147,6 +147,13 @@ class SandeshMessageStatistics(object):
                 else:
                     msg_stats.messages_sent_dropped_wrong_client_sm_state = 1
                     msg_stats.bytes_sent_dropped_wrong_client_sm_state = nbytes
+            elif drop_reason is SandeshTxDropReason.BufferFull:
+                if msg_stats.messages_sent_dropped_buffer_full:
+                    msg_stats.messages_sent_dropped_buffer_full += 1
+                    msg_stats.bytes_sent_dropped_buffer_full += nbytes
+                else:
+                    msg_stats.messages_sent_dropped_buffer_full = 1
+                    msg_stats.bytes_sent_dropped_buffer_full = nbytes
             else:
                 assert 0, 'Unhandled Tx drop reason <%s>' % (str(drop_reason))
     # end _update_tx_stats_internal

Juniper/contrail-common

sandesh/library/cpp/sandesh_statistics.cc
@@ -42,6 +42,10 @@ static void UpdateSandeshMessageStatsDrops(SandeshMessageStats *smstats,
             smstats->messages_sent_dropped_validation_failed++;
             smstats->bytes_sent_dropped_validation_failed += bytes;
             break;
+          case SandeshTxDropReason::RatelimitDrop:
+            smstats->messages_sent_dropped_rate_limited++;
+            smstats->bytes_sent_dropped_rate_limited += bytes;
+            break;
           case SandeshTxDropReason::QueueLevel:
             smstats->messages_sent_dropped_queue_level++;
             smstats->bytes_sent_dropped_queue_level += bytes;

sandesh/library/python/pysandesh/sandesh_stats.py
@@ -147,6 +147,13 @@ class SandeshMessageStatistics(object):
                 else:
                     msg_stats.messages_sent_dropped_wrong_client_sm_state = 1
                     msg_stats.bytes_sent_dropped_wrong_client_sm_state = nbytes
+            elif drop_reason is SandeshTxDropReason.RatelimitDrop:
+                if msg_stats.messages_sent_dropped_rate_limited:
+                    msg_stats.messages_sent_dropped_rate_limited += 1
+                    msg_stats.bytes_sent_dropped_rate_limited += nbytes
+                else:
+                    msg_stats.messages_sent_dropped_rate_limited = 1
+                    msg_stats.bytes_sent_dropped_rate_limited = nbytes
             else:
                 assert 0, 'Unhandled Tx drop reason <%s>' % (str(drop_reason))
     # end _update_tx_stats_internal