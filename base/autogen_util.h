Juniper/contrail-controller

Use contrail_rapidjson as namespace to allow multiple versions
Different versions rapidjson code which is headers only though still needs to
be kept separately as they contain templates. When instantiated, class from
one version can collide with another and produce undesired and unexpected
outcome.

Use a separate namespace contrail_rapidjson as a fix in all of contrail code

Change-Id: I9a246e2ad8552a29f39422f7f66690fa676262ae
Partial-Bug: #1632470

src/base/autogen_util.h
@@ -15,7 +15,7 @@
 #include "rapidjson/rapidjson.h"
 #include "rapidjson/document.h"
 
-using namespace rapidjson;
+using namespace RAPIDJSON_NAMESPACE;
 using namespace pugi;
 using namespace std;
 
@@ -28,7 +28,7 @@ namespace autogen {
 
 // Json Parse routines
 
-static bool ParseString(const rapidjson::Value &node, std::string *s) {
+static bool ParseString(const RAPIDJSON_NAMESPACE::Value &node, std::string *s) {
     if (node.IsString()) {
         *s = node.GetString();
         return true;
@@ -36,19 +36,19 @@ static bool ParseString(const rapidjson::Value &node, std::string *s) {
 
     std::stringstream ss;
     switch (node.GetType()) {
-    case rapidjson::kNullType:
+    case RAPIDJSON_NAMESPACE::kNullType:
         *s = "null";
         break;
-    case rapidjson::kTrueType:
+    case RAPIDJSON_NAMESPACE::kTrueType:
         *s = "true";
         break;
-    case rapidjson::kFalseType:
+    case RAPIDJSON_NAMESPACE::kFalseType:
         *s = "false";
         break;
-    case rapidjson::kStringType:
+    case RAPIDJSON_NAMESPACE::kStringType:
         *s = node.GetString();
         break;
-    case rapidjson::kNumberType:
+    case RAPIDJSON_NAMESPACE::kNumberType:
         if (node.IsUint())
             ss << node.GetUint();
         else if (node.IsInt())
@@ -61,9 +61,9 @@ static bool ParseString(const rapidjson::Value &node, std::string *s) {
             ss << node.GetDouble();
         *s = ss.str();
         break;
-    case rapidjson::kObjectType:
+    case RAPIDJSON_NAMESPACE::kObjectType:
         return false;
-    case rapidjson::kArrayType:
+    case RAPIDJSON_NAMESPACE::kArrayType:
         return false;
     }
     return true;
@@ -156,7 +156,7 @@ static std::string FormatTime(const time_t *valuep) {
 }
 
 // Json Parse routines
-static bool ParseInteger(const rapidjson::Value &node, int *valuep) {
+static bool ParseInteger(const RAPIDJSON_NAMESPACE::Value &node, int *valuep) {
     if (node.IsString())
         return ParseInteger(node.GetString(), valuep);
     if (!node.IsInt())
@@ -165,7 +165,7 @@ static bool ParseInteger(const rapidjson::Value &node, int *valuep) {
     return true;
 }
 
-static bool ParseUnsignedLong(const rapidjson::Value &node, uint64_t *valuep) {
+static bool ParseUnsignedLong(const RAPIDJSON_NAMESPACE::Value &node, uint64_t *valuep) {
     if (node.IsString())
         return ParseUnsignedLong(node.GetString(), valuep);
     if (!node.IsUint64())
@@ -174,7 +174,7 @@ static bool ParseUnsignedLong(const rapidjson::Value &node, uint64_t *valuep) {
     return true;
 }
 
-static bool ParseBoolean(const rapidjson::Value &node, bool *valuep) {
+static bool ParseBoolean(const RAPIDJSON_NAMESPACE::Value &node, bool *valuep) {
     if (node.IsString())
         return ParseBoolean(node.GetString(), valuep);
     if (!node.IsBool())
@@ -183,7 +183,7 @@ static bool ParseBoolean(const rapidjson::Value &node, bool *valuep) {
     return true;
 }
 
-static bool ParseDateTime(const rapidjson::Value &node, time_t *valuep) {
+static bool ParseDateTime(const RAPIDJSON_NAMESPACE::Value &node, time_t *valuep) {
     if (!node.IsString())
         return false;
     string value(node.GetString());
@@ -198,7 +198,7 @@ static bool ParseDateTime(const rapidjson::Value &node, time_t *valuep) {
     return true;
 }
 
-static bool ParseTime(const rapidjson::Value &node, time_t *valuep) {
+static bool ParseTime(const RAPIDJSON_NAMESPACE::Value &node, time_t *valuep) {
     if (!node.IsString())
         return false;
     string value(node.GetString());

src/base/misc_utils.cc
@@ -98,25 +98,25 @@ bool MiscUtils::GetBuildInfo(BuildModule id, const string &build_info,
     string build_num;
 
     bool ret = GetContrailVersionInfo(id, rpm_version, build_num);
-    rapidjson::Document d;
+    RAPIDJSON_NAMESPACE::Document d;
     if (d.Parse<0>(const_cast<char *>(build_info.c_str())).HasParseError()) {
         result = build_info;
         return false;
     }
-    rapidjson::Value& fields = d["build-info"];
+    RAPIDJSON_NAMESPACE::Value& fields = d["build-info"];
     if (!fields.IsArray()) {
         result = build_info;
         return false;
     }
 
-    rapidjson::Value v;
+    RAPIDJSON_NAMESPACE::Value v;
     fields[0u].AddMember("build-id",
         v.SetString(rpm_version.c_str(), d.GetAllocator()), d.GetAllocator());
     fields[0u].AddMember("build-number",
         v.SetString(build_num.c_str(), d.GetAllocator()), d.GetAllocator());
 
-    rapidjson::StringBuffer strbuf;
-    rapidjson::Writer<rapidjson::StringBuffer> writer(strbuf);
+    RAPIDJSON_NAMESPACE::StringBuffer strbuf;
+    RAPIDJSON_NAMESPACE::Writer<RAPIDJSON_NAMESPACE::StringBuffer> writer(strbuf);
     d.Accept(writer);
     result = strbuf.GetString();
     return ret;

Juniper/contrail-common

base/autogen_util.h
@@ -15,7 +15,7 @@
 #include "rapidjson/rapidjson.h"
 #include "rapidjson/document.h"
 
-using namespace rapidjson;
+using namespace contrail_rapidjson;
 using namespace pugi;
 using namespace std;
 
@@ -28,7 +28,7 @@ namespace autogen {
 
 // Json Parse routines
 
-static bool ParseString(const rapidjson::Value &node, std::string *s) {
+static bool ParseString(const contrail_rapidjson::Value &node, std::string *s) {
     if (node.IsString()) {
         *s = node.GetString();
         return true;
@@ -36,19 +36,19 @@ static bool ParseString(const rapidjson::Value &node, std::string *s) {
 
     std::stringstream ss;
     switch (node.GetType()) {
-    case rapidjson::kNullType:
+    case contrail_rapidjson::kNullType:
         *s = "null";
         break;
-    case rapidjson::kTrueType:
+    case contrail_rapidjson::kTrueType:
         *s = "true";
         break;
-    case rapidjson::kFalseType:
+    case contrail_rapidjson::kFalseType:
         *s = "false";
         break;
-    case rapidjson::kStringType:
+    case contrail_rapidjson::kStringType:
         *s = node.GetString();
         break;
-    case rapidjson::kNumberType:
+    case contrail_rapidjson::kNumberType:
         if (node.IsUint())
             ss << node.GetUint();
         else if (node.IsInt())
@@ -61,9 +61,9 @@ static bool ParseString(const rapidjson::Value &node, std::string *s) {
             ss << node.GetDouble();
         *s = ss.str();
         break;
-    case rapidjson::kObjectType:
+    case contrail_rapidjson::kObjectType:
         return false;
-    case rapidjson::kArrayType:
+    case contrail_rapidjson::kArrayType:
         return false;
     }
     return true;
@@ -156,7 +156,7 @@ static std::string FormatTime(const time_t *valuep) {
 }
 
 // Json Parse routines
-static bool ParseInteger(const rapidjson::Value &node, int *valuep) {
+static bool ParseInteger(const contrail_rapidjson::Value &node, int *valuep) {
     if (node.IsString())
         return ParseInteger(node.GetString(), valuep);
     if (!node.IsInt())
@@ -165,7 +165,7 @@ static bool ParseInteger(const rapidjson::Value &node, int *valuep) {
     return true;
 }
 
-static bool ParseUnsignedLong(const rapidjson::Value &node, uint64_t *valuep) {
+static bool ParseUnsignedLong(const contrail_rapidjson::Value &node, uint64_t *valuep) {
     if (node.IsString())
         return ParseUnsignedLong(node.GetString(), valuep);
     if (!node.IsUint64())
@@ -174,7 +174,7 @@ static bool ParseUnsignedLong(const rapidjson::Value &node, uint64_t *valuep) {
     return true;
 }
 
-static bool ParseBoolean(const rapidjson::Value &node, bool *valuep) {
+static bool ParseBoolean(const contrail_rapidjson::Value &node, bool *valuep) {
     if (node.IsString())
         return ParseBoolean(node.GetString(), valuep);
     if (!node.IsBool())
@@ -183,7 +183,7 @@ static bool ParseBoolean(const rapidjson::Value &node, bool *valuep) {
     return true;
 }
 
-static bool ParseDateTime(const rapidjson::Value &node, time_t *valuep) {
+static bool ParseDateTime(const contrail_rapidjson::Value &node, time_t *valuep) {
     if (!node.IsString())
         return false;
     string value(node.GetString());
@@ -198,7 +198,7 @@ static bool ParseDateTime(const rapidjson::Value &node, time_t *valuep) {
     return true;
 }
 
-static bool ParseTime(const rapidjson::Value &node, time_t *valuep) {
+static bool ParseTime(const contrail_rapidjson::Value &node, time_t *valuep) {
     if (!node.IsString())
         return false;
     string value(node.GetString());

base/misc_utils.cc
@@ -98,25 +98,25 @@ bool MiscUtils::GetBuildInfo(BuildModule id, const string &build_info,
     string build_num;
 
     bool ret = GetContrailVersionInfo(id, rpm_version, build_num);
-    rapidjson::Document d;
+    contrail_rapidjson::Document d;
     if (d.Parse<0>(const_cast<char *>(build_info.c_str())).HasParseError()) {
         result = build_info;
         return false;
     }
-    rapidjson::Value& fields = d["build-info"];
+    contrail_rapidjson::Value& fields = d["build-info"];
     if (!fields.IsArray()) {
         result = build_info;
         return false;
     }
 
-    rapidjson::Value v;
+    contrail_rapidjson::Value v;
     fields[0u].AddMember("build-id",
         v.SetString(rpm_version.c_str(), d.GetAllocator()), d.GetAllocator());
     fields[0u].AddMember("build-number",
         v.SetString(build_num.c_str(), d.GetAllocator()), d.GetAllocator());
 
-    rapidjson::StringBuffer strbuf;
-    rapidjson::Writer<rapidjson::StringBuffer> writer(strbuf);
+    contrail_rapidjson::StringBuffer strbuf;
+    contrail_rapidjson::Writer<contrail_rapidjson::StringBuffer> writer(strbuf);
     d.Accept(writer);
     result = strbuf.GetString();
     return ret;