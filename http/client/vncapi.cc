Juniper/contrail-controller

Use contrail_rapidjson as namespace to allow multiple versions
Different versions rapidjson code which is headers only though still needs to
be kept separately as they contain templates. When instantiated, class from
one version can collide with another and produce undesired and unexpected
outcome.

Use a separate namespace contrail_rapidjson as a fix in all of contrail code

Change-Id: I9a246e2ad8552a29f39422f7f66690fa676262ae
Partial-Bug: #1632470

src/http/client/vncapi.cc
@@ -20,7 +20,7 @@
 #include "vncapi.h"
 
 RespBlock::RespBlock(HttpConnection *c, std::string uri,
-            boost::function<void(rapidjson::Document&,
+            boost::function<void(RAPIDJSON_NAMESPACE::Document&,
                 boost::system::error_code&, std::string, int, std::string,
                 std::map<std::string, std::string>*)> cb):
         conn_(c), uri_(uri), cb_(cb)
@@ -66,7 +66,7 @@ RespBlock::ShowDetails()
         << conn_->Reason() << "(" << conn_->Status() << ")\n";
 }
 
-boost::function<void(rapidjson::Document&, boost::system::error_code&,
+boost::function<void(RAPIDJSON_NAMESPACE::Document&, boost::system::error_code&,
         std::string, int, std::string, std::map<std::string, std::string>*)>
 RespBlock::GetCallBack()
 {
@@ -132,7 +132,7 @@ VncApi::CondTest(std::string s)
 }
 
 std::string VncApi::GetToken(RespBlock *rb) const {
-    rapidjson::Document jdoc;
+    RAPIDJSON_NAMESPACE::Document jdoc;
     jdoc.Parse<0>(rb->GetBody().c_str());
 
     if (!jdoc.IsObject() || !jdoc.HasMember("access"))
@@ -160,7 +160,7 @@ VncApi::KsRespHandler(RespBlock *rb, RespBlock *orb, std::string &str,
     if (client_) {
         if (str == "") {
             if (rb->GetConnection()->Status() == 200) {
-                rapidjson::Document jdoc;
+                RAPIDJSON_NAMESPACE::Document jdoc;
                 jdoc.Parse<0>(rb->GetBody().c_str());
 
                 std::string token = GetToken(rb);
@@ -266,7 +266,7 @@ void
 VncApi::GetConfig(std::string type, std::vector<std::string> ids,
         std::vector<std::string> filters, std::vector<std::string> parents,
         std::vector<std::string> refs, std::vector<std::string> fields,
-        boost::function<void(rapidjson::Document&,
+        boost::function<void(RAPIDJSON_NAMESPACE::Document&,
             boost::system::error_code &ec, std::string version, int status,
             std::string reason,
             std::map<std::string, std::string> *headers)> cb)
@@ -298,7 +298,7 @@ VncApi::RespHandler(RespBlock *rb, std::string &str,
                 rb->Clear();
                 Reauthenticate(rb);
             } else {
-                rapidjson::Document jdoc;
+                RAPIDJSON_NAMESPACE::Document jdoc;
                 if (rb->GetConnection()->Status() == 200)
                     jdoc.Parse<0>(rb->GetBody().c_str());
                 rb->GetCallBack()(jdoc, ec,

src/http/client/vncapi.h
@@ -28,7 +28,7 @@ struct VncApiConfig {
 class RespBlock {
     public:
     RespBlock(HttpConnection *c, std::string uri,
-            boost::function<void(rapidjson::Document&,
+            boost::function<void(RAPIDJSON_NAMESPACE::Document&,
                 boost::system::error_code&, std::string, int, std::string,
                 std::map<std::string, std::string>*)> cb);
     HttpConnection *GetConnection();
@@ -37,13 +37,13 @@ class RespBlock {
     std::string GetBody();
     std::string GetUri();
     void ShowDetails();
-    boost::function<void(rapidjson::Document&,
+    boost::function<void(RAPIDJSON_NAMESPACE::Document&,
             boost::system::error_code&, std::string, int, std::string,
             std::map<std::string, std::string>*)> GetCallBack();
     private:
     HttpConnection* conn_;
     std::string uri_;
-    boost::function<void(rapidjson::Document&,
+    boost::function<void(RAPIDJSON_NAMESPACE::Document&,
             boost::system::error_code&, std::string, int, std::string,
             std::map<std::string, std::string>*)> cb_;
     std::string body_;
@@ -78,7 +78,7 @@ class VncApi : public boost::enable_shared_from_this<VncApi> {
     void GetConfig(std::string type, std::vector<std::string> ids,
             std::vector<std::string> filters, std::vector<std::string> parents,
             std::vector<std::string> refs, std::vector<std::string> fields,
-            boost::function<void(rapidjson::Document&,
+            boost::function<void(RAPIDJSON_NAMESPACE::Document&,
                 boost::system::error_code &ec, std::string version, int status,
                 std::string reason,
                 std::map<std::string, std::string> *headers)> cb);

src/http/client/vncapi_test.cc
@@ -91,17 +91,17 @@ class Controller {
         std::cout  << "\n" << x << " all Done waiting...\n";
         return false;
     }
-    void UDCHandler(rapidjson::Document &jdoc, boost::system::error_code &ec,
+    void UDCHandler(RAPIDJSON_NAMESPACE::Document &jdoc, boost::system::error_code &ec,
             std::string version, int status, std::string reason,
             std::map<std::string, std::string> *headers) {
 
         if (jdoc.IsObject() && jdoc.HasMember("global-system-configs")) {
-            for (rapidjson::SizeType j=0;
+            for (RAPIDJSON_NAMESPACE::SizeType j=0;
                         j < jdoc["global-system-configs"].Size(); j++) {
-                const rapidjson::Value& gsc = jdoc["global-system-configs"][j]
+                const RAPIDJSON_NAMESPACE::Value& gsc = jdoc["global-system-configs"][j]
                             ["user_defined_counter"]["counter"];
                 if (gsc.IsArray()) {
-                    for (rapidjson::SizeType i = 0; i < gsc.Size(); i++)
+                    for (RAPIDJSON_NAMESPACE::SizeType i = 0; i < gsc.Size(); i++)
                         std::cout << "\nname: " << gsc[i]["name"].GetString()
                             << "\npattern: " << gsc[i]["pattern"].GetString()
                             << "\n";

Juniper/contrail-common

http/client/vncapi.cc
@@ -20,7 +20,7 @@
 #include "vncapi.h"
 
 RespBlock::RespBlock(HttpConnection *c, std::string uri,
-            boost::function<void(rapidjson::Document&,
+            boost::function<void(contrail_rapidjson::Document&,
                 boost::system::error_code&, std::string, int, std::string,
                 std::map<std::string, std::string>*)> cb):
         conn_(c), uri_(uri), cb_(cb)
@@ -66,7 +66,7 @@ RespBlock::ShowDetails()
         << conn_->Reason() << "(" << conn_->Status() << ")\n";
 }
 
-boost::function<void(rapidjson::Document&, boost::system::error_code&,
+boost::function<void(contrail_rapidjson::Document&, boost::system::error_code&,
         std::string, int, std::string, std::map<std::string, std::string>*)>
 RespBlock::GetCallBack()
 {
@@ -132,7 +132,7 @@ VncApi::CondTest(std::string s)
 }
 
 std::string VncApi::GetToken(RespBlock *rb) const {
-    rapidjson::Document jdoc;
+    contrail_rapidjson::Document jdoc;
     jdoc.Parse<0>(rb->GetBody().c_str());
 
     if (!jdoc.IsObject() || !jdoc.HasMember("access"))
@@ -160,7 +160,7 @@ VncApi::KsRespHandler(RespBlock *rb, RespBlock *orb, std::string &str,
     if (client_) {
         if (str == "") {
             if (rb->GetConnection()->Status() == 200) {
-                rapidjson::Document jdoc;
+                contrail_rapidjson::Document jdoc;
                 jdoc.Parse<0>(rb->GetBody().c_str());
 
                 std::string token = GetToken(rb);
@@ -266,7 +266,7 @@ void
 VncApi::GetConfig(std::string type, std::vector<std::string> ids,
         std::vector<std::string> filters, std::vector<std::string> parents,
         std::vector<std::string> refs, std::vector<std::string> fields,
-        boost::function<void(rapidjson::Document&,
+        boost::function<void(contrail_rapidjson::Document&,
             boost::system::error_code &ec, std::string version, int status,
             std::string reason,
             std::map<std::string, std::string> *headers)> cb)
@@ -298,7 +298,7 @@ VncApi::RespHandler(RespBlock *rb, std::string &str,
                 rb->Clear();
                 Reauthenticate(rb);
             } else {
-                rapidjson::Document jdoc;
+                contrail_rapidjson::Document jdoc;
                 if (rb->GetConnection()->Status() == 200)
                     jdoc.Parse<0>(rb->GetBody().c_str());
                 rb->GetCallBack()(jdoc, ec,

http/client/vncapi.h
@@ -28,7 +28,7 @@ struct VncApiConfig {
 class RespBlock {
     public:
     RespBlock(HttpConnection *c, std::string uri,
-            boost::function<void(rapidjson::Document&,
+            boost::function<void(contrail_rapidjson::Document&,
                 boost::system::error_code&, std::string, int, std::string,
                 std::map<std::string, std::string>*)> cb);
     HttpConnection *GetConnection();
@@ -37,13 +37,13 @@ class RespBlock {
     std::string GetBody();
     std::string GetUri();
     void ShowDetails();
-    boost::function<void(rapidjson::Document&,
+    boost::function<void(contrail_rapidjson::Document&,
             boost::system::error_code&, std::string, int, std::string,
             std::map<std::string, std::string>*)> GetCallBack();
     private:
     HttpConnection* conn_;
     std::string uri_;
-    boost::function<void(rapidjson::Document&,
+    boost::function<void(contrail_rapidjson::Document&,
             boost::system::error_code&, std::string, int, std::string,
             std::map<std::string, std::string>*)> cb_;
     std::string body_;
@@ -78,7 +78,7 @@ class VncApi : public boost::enable_shared_from_this<VncApi> {
     void GetConfig(std::string type, std::vector<std::string> ids,
             std::vector<std::string> filters, std::vector<std::string> parents,
             std::vector<std::string> refs, std::vector<std::string> fields,
-            boost::function<void(rapidjson::Document&,
+            boost::function<void(contrail_rapidjson::Document&,
                 boost::system::error_code &ec, std::string version, int status,
                 std::string reason,
                 std::map<std::string, std::string> *headers)> cb);

http/client/vncapi_test.cc
@@ -91,17 +91,17 @@ class Controller {
         std::cout  << "\n" << x << " all Done waiting...\n";
         return false;
     }
-    void UDCHandler(rapidjson::Document &jdoc, boost::system::error_code &ec,
+    void UDCHandler(contrail_rapidjson::Document &jdoc, boost::system::error_code &ec,
             std::string version, int status, std::string reason,
             std::map<std::string, std::string> *headers) {
 
         if (jdoc.IsObject() && jdoc.HasMember("global-system-configs")) {
-            for (rapidjson::SizeType j=0;
+            for (contrail_rapidjson::SizeType j=0;
                         j < jdoc["global-system-configs"].Size(); j++) {
-                const rapidjson::Value& gsc = jdoc["global-system-configs"][j]
+                const contrail_rapidjson::Value& gsc = jdoc["global-system-configs"][j]
                             ["user_defined_counter"]["counter"];
                 if (gsc.IsArray()) {
-                    for (rapidjson::SizeType i = 0; i < gsc.Size(); i++)
+                    for (contrail_rapidjson::SizeType i = 0; i < gsc.Size(); i++)
                         std::cout << "\nname: " << gsc[i]["name"].GetString()
                             << "\npattern: " << gsc[i]["pattern"].GetString()
                             << "\n";