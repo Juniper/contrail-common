Juniper/contrail-controller

Analytics assert happen because lua return false. Anlytics hope an array is returned, but lua return false in error senario. This is translated to nil.
Change-Id: I2d559df737cbd48bd01621df906dcb9ed9bd2032
Closes-bug: 1698243

src/base/task.cc
@@ -1504,6 +1504,7 @@ void TaskEntry::TaskExited(Task *t, TaskGroup *group) {
     
     run_count_--;
     stats_.total_tasks_completed_++;
+    stats_.latest_exit_time_ = UTCTimestampUsec();
     group->TaskExited(t);
 
     if (!group->run_count_ && !run_count_) {
@@ -1595,6 +1596,7 @@ void TaskEntry::GetSandeshData(SandeshTaskEntry *resp) const {
     resp->set_instance_id(task_instance_);
     resp->set_tasks_created(stats_.enqueue_count_);
     resp->set_total_tasks_completed(stats_.total_tasks_completed_);
+    resp->set_latest_exit_time(stats_.latest_exit_time_);
     resp->set_tasks_running(run_count_);
     resp->set_waitq_size(waitq_.size());
     resp->set_deferq_size(deferq_->size());

src/base/task.h
@@ -55,6 +55,7 @@ struct TaskStats {
     int     defer_count_;               // #Entries in deferq
     uint64_t enqueue_count_;            // #Tasks enqueued
     uint64_t total_tasks_completed_;    // #Total tasks ran
+    uint64_t latest_exit_time_;        // #Time stamp of latest exist
 };
 
 struct TaskExclusion {

Juniper/contrail-common

base/task.cc
@@ -1504,6 +1504,7 @@ void TaskEntry::TaskExited(Task *t, TaskGroup *group) {
     
     run_count_--;
     stats_.total_tasks_completed_++;
+    stats_.last_exit_time_ = UTCTimestampUsec();
     group->TaskExited(t);
 
     if (!group->run_count_ && !run_count_) {
@@ -1598,6 +1599,7 @@ void TaskEntry::GetSandeshData(SandeshTaskEntry *resp) const {
     resp->set_tasks_running(run_count_);
     resp->set_waitq_size(waitq_.size());
     resp->set_deferq_size(deferq_->size());
+    resp->set_last_exit_time(stats_.last_exit_time_);
 }
 void TaskGroup::GetSandeshData(SandeshTaskGroup *resp, bool summary) const {
     if (total_run_time_)

base/task.h
@@ -55,6 +55,7 @@ struct TaskStats {
     int     defer_count_;               // #Entries in deferq
     uint64_t enqueue_count_;            // #Tasks enqueued
     uint64_t total_tasks_completed_;    // #Total tasks ran
+    uint64_t last_exit_time_;           // #Time stamp of latest exist
 };
 
 struct TaskExclusion {