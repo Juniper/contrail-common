1ed407413e93eaea0b6cd4018a4e9e173586f505
U-JNPR\sagarc <sagarc@juniper.net>
2017-12-04 01:18:38 -0800

    port base module in windows  Original working changes and change history:  https://github.com/sagarkchitnis/windows/commits/master/controller/base/
     subsequent changes in https://github.com/codilime/contrail-controller/tree/windows3.1/
    
     Other minor changes TBD.
    
    Change-Id: I49c6dd47e6a972a4c0c7bdfb5fd52c4b6c7c93ff
    Partial-Bug: #1736099

--- base/factory.h
+++ base/factory.h
@@ -7,6 +7,9 @@
 
 #include <boost/function.hpp>
 #include <boost/functional/factory.hpp>
+#ifdef _WIN32
+#include <boost/functional/forward_adapter.hpp>
+#endif
 #include <boost/type_traits/is_same.hpp>
 #include <boost/utility/enable_if.hpp>
 
@@ -27,16 +30,43 @@ class Factory {
 
 #include "base/factory_macros.h"
 
+#ifdef _WIN32
+#define FACTORY_N0_STATIC_REGISTER(_Factory, _BaseType, _TypeImpl)\
+static void _Factory ## _TypeImpl ## Register () {\
+    _Factory::Register<_BaseType>(boost::factory<_TypeImpl *>());\
+}\
+MODULE_INITIALIZER(_Factory ## _TypeImpl ## Register)
+#else
 #define FACTORY_STATIC_REGISTER(_Factory, _BaseType, _TypeImpl)\
 static void _Factory ## _TypeImpl ## Register () {\
     _Factory::Register<_BaseType>(boost::factory<_TypeImpl *>());\
 }\
 MODULE_INITIALIZER(_Factory ## _TypeImpl ## Register)
+#endif
+
+
+#ifdef _WIN32
+#define FACTORY_STATIC_REGISTER(_Factory, _BaseType, _TypeImpl)\
+static void _Factory ## _TypeImpl ## Register () {\
+    _Factory::Register<_BaseType>(boost::forward_adapter<boost::factory<_TypeImpl *> >(boost::factory<_TypeImpl *>()));\
+}\
+MODULE_INITIALIZER(_Factory ## _TypeImpl ## Register)
+#endif
+
 
 #define FACTORY_PARAM_STATIC_REGISTER(_Factory, _BaseType, _Param, _TypeImpl)\
 static void _Factory ## _TypeImpl ## Register () {\
-    _Factory::Register<_BaseType, _Param>(boost::factory<_TypeImpl *>());\
+
+#ifdef _WIN32
+    _Factory::Register<_BaseType, _Param>(boost::forward_adapter<boost::factory<_TypeImpl *> >(boost::factory<_TypeImpl *>())); \
 }\
 MODULE_INITIALIZER(_Factory ## _TypeImpl ## Register)
+#else
+    _Factory::Register<_BaseType, _Param>(boost::factory<_TypeImpl *>());\
+        }\
+MODULE_INITIALIZER(_Factory ## _TypeImpl ## Register)
+#endif
+
 
 #endif
+
