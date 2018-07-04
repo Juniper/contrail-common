34bd7633d19d95e699059efe65578f9410a0dec7
zcui <zcui@juniper.net>
2018-07-03 22:38:10 -0700

    Add contrail-collector to zookeeper
    Problem description:
      To check redis down or not, current uve server implement is like:
      (1) Try to connect configured redis server, if failed return false
      (2) else read contrail-collector from redis db, if not found, return
          false
      But one scenario is redis/contrail-collector pair down, we should
      not consider this as fault since no any useful date in DB.
      To correct this, we redesign it. We will add contrail-collecor to
      zookeeper.
      The znode is like
           /contrail-collector/{collector_ip1}
           /contrail-collector/{collector_ip2}
           ......
      /contrail-collector is PERSISTENT znode, {collector_ip1} is EPHEMERAL
      znode, and znode value is
           {hosename}:Analytics:contrail-collecor:0
    
    This is first commit to expand zookeeper C++ API
    
    Change-Id: I4090bbe68614c5e4fa673ba3ceb065cc4c2e6be3
    Partial-bug: 1733027

--- zookeeper/zookeeper_client_impl.h
+++ zookeeper/zookeeper_client_impl.h
@@ -26,7 +26,8 @@ class ZookeeperClientImpl {
     void Shutdown();
     bool Reconnect();
     bool IsConnected() const;
-    int CreateNodeSync(const char *path, const char *value, int *err);
+    bool CreateNode(const char * path, int flag = 0);
+    int CreateNodeSync(const char *path, const char *value, int *err, int flag = 0);
     int GetNodeDataSync(const char *path, char *buf, int *buf_len, int *err);
     int DeleteNodeSync(const char *path, int *err);
     std::string Name() const;
