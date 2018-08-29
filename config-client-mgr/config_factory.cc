babc75b8e772c39604364caf6e3be1893bc3a03c
Mahesh Sivakumar <maheshskumar@juniper.net>
2018-08-29 12:51:43 -0700

    ETCD control-node changes: Changes to make control-node use etcd client library to read config. This will be done in addition to the existing support for reading from Cassandra. Based on the input in conf file, one of the two will be used.
    Change-Id: I11fd1051bd3239729d3fefcbf2aec8877311ece6
    Depends-On: Ibcc1c3d3c192ab093169bfdd3c3845cc63cd0df9
    Partial-Bug: 1778805

--- config-client-mgr/config_factory.cc
+++ config-client-mgr/config_factory.cc
@@ -17,8 +17,22 @@ FACTORY_STATIC_REGISTER(ConfigFactory, ConfigCassandraPartition,
 FACTORY_STATIC_REGISTER(ConfigFactory, ConfigCassandraClient,
                         ConfigCassandraClient);
 
+#include "config_amqp_client.h"
+FACTORY_STATIC_REGISTER(ConfigFactory, ConfigAmqpChannel, ConfigAmqpChannel);
+
+#ifdef ETCD_INCL
+#include "config_etcd_client.h"
+FACTORY_STATIC_REGISTER(ConfigFactory, ConfigEtcdPartition,
+                        ConfigEtcdPartition);
+#include "config_etcd_client.h"
+FACTORY_STATIC_REGISTER(ConfigFactory, ConfigEtcdClient,
+                        ConfigEtcdClient);
+#endif
+
 #include "database/cassandra/cql/cql_if.h"
 FACTORY_STATIC_REGISTER(ConfigFactory, CqlIf, CqlIf);
 
-#include "config_amqp_client.h"
-FACTORY_STATIC_REGISTER(ConfigFactory, ConfigAmqpChannel, ConfigAmqpChannel);
+#ifdef ETCD_INCL
+#include "database/etcd/eql_if.h"
+FACTORY_STATIC_REGISTER(ConfigFactory, EtcdIf, EtcdIf);
+#endif
