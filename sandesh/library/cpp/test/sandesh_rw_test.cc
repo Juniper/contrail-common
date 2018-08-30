180a9e90cb9808b37fde5e95d1c8a891a11ac006
arvindvis <arvindv@juniper.net>
2018-08-29 21:44:36 -0700

    JSON encoding of the sandesh UVE messages
    This fix adds the file wihch provides implementation of
    transport API's exposed. The transport API's will be called
    by the generated sandesh code.
    
    UT added for the JSON generator. Currently the validity of
    the JSON was checked with online tools. Holding the
    verification part until we export a JSON library to sandesh.
    Partial-Bug: 1789312
    
    Change-Id: I1a8ddcdbccb5e8a899c7f45f3ba805eed65b5766

--- sandesh/library/cpp/test/sandesh_rw_test.cc
+++ sandesh/library/cpp/test/sandesh_rw_test.cc
@@ -21,6 +21,7 @@
 #include "base/util.h"
 
 #include <sandesh/protocol/TXMLProtocol.h>
+#include <sandesh/protocol/TJSONProtocol.h>
 #include <sandesh/protocol/TBinaryProtocol.h>
 #include <sandesh/transport/TBufferTransports.h>
 
@@ -74,7 +75,7 @@ protected:
         for (uint32_t i = 0; i < test_list_size; i++) {
             // (0, "0"), (1, "1"), (2, "2"), (3, "3"), (4, "4")
             test_basic_type_map.insert(std::pair<int32_t, std::string>
-                                       (i, integerToString(i)));
+                                       (i, "a"+integerToString(i)));
         }
         wstruct_test_.set_basicTypeMapTest(test_basic_type_map);
         // Create a map of <int32_t, SandeshListTestElement>
@@ -121,8 +122,88 @@ protected:
         EXPECT_EQ(wstruct_test_, rstruct_test_);
     }
 
+    void SandeshJsonReadWriteProcess(boost::shared_ptr<TMemoryBuffer> btrans,
+            boost::shared_ptr<TProtocol> prot) {
+        uint8_t *buffer;
+        uint32_t offset, wxfer, rxfer;
+        wjson_struct_test_.set_name("abc");
+        // Initialize the struct
+        wjson_struct_test_.set_i32Test(test_i32);
+        // Create a vector of SandeshStructTest
+        std::vector<SandeshListTestElement> test_list;
+        for (uint32_t i = 0; i < test_list_size; i++) {
+            SandeshListTestElement tmp;
+            tmp.set_i32Elem(i);
+            test_list.push_back(tmp); // 0, 1, 2, 3, 4
+        }
+        wjson_struct_test_.set_listTest(test_list);
+        // Create a vector of int32_t
+        std::vector<int32_t> test_basic_type_list;
+        for (uint32_t i = 0; i < test_list_size; i++) {
+            test_basic_type_list.push_back(i); // 0, 1, 2, 3, 4
+        }
+        wjson_struct_test_.set_basicTypeListTest(test_basic_type_list);
+        // Create a vector of uuid
+        std::vector<boost::uuids::uuid> test_uuid_list;
+        for (uint32_t i = 0; i < test_list_size; i++) {
+            boost::uuids::uuid uuid_temp =
+                {0x00+i,0x00+i,0x01+i,0x01+i,0x02+i,0x02+i,0x03+i,0x03+i,
+                 0x04+i,0x04+i,0x05+i,0x05+i,0x06+i,0x06+i,0x07+i,0x07+i};
+            test_uuid_list.push_back(uuid_temp);
+        }
+        wjson_struct_test_.set_uuidListTest(test_uuid_list);
+        // Create a map of <int32_t, string>
+        std::map<int32_t, std::string> test_basic_type_map;
+        for (uint32_t i = 0; i < test_list_size; i++) {
+            // (0, "0"), (1, "1"), (2, "2"), (3, "3"), (4, "4")
+            test_basic_type_map.insert(std::pair<int32_t, std::string>
+                                       (i, "a"+integerToString(i)));
+        }
+        wjson_struct_test_.set_basicTypeMapTest(test_basic_type_map);
+        // Create a map of <int32_t, SandeshListTestElement>
+        std::map<int32_t, SandeshListTestElement> test_complex_type_map;
+        for (uint32_t i = 0; i < test_list_size; i++) {
+            SandeshListTestElement tmp;
+            tmp.set_i32Elem(i);
+            test_complex_type_map.insert(std::pair<int32_t, SandeshListTestElement>
+                                         (i, tmp));
+        }
+        wjson_struct_test_.set_complexTypeMapTest(test_complex_type_map);
+        wjson_struct_test_.set_u16Test(65535);
+        wjson_struct_test_.set_u32Test(4294967295u);
+        wjson_struct_test_.set_u64Test(18446744073709551615ull);
+        wjson_struct_test_.set_ipv4Test(4294967295u);
+        boost::uuids::uuid uuid_test =
+             {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,
+              0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f};
+        wjson_struct_test_.set_uuidTest(uuid_test);
+        boost::system::error_code ec;
+        wjson_struct_test_.set_ipaddrv4Test(address::from_string("10.10.1.1", ec));
+        wjson_struct_test_.set_ipaddrv6Test(address::from_string("2001:db8::2:1", ec));
+        std::vector<address> ipaddrs;
+        ipaddrs.push_back(address::from_string("192.168.1.10", ec));
+        ipaddrs.push_back(address::from_string("2001:dc8::1:2", ec));
+        wjson_struct_test_.set_ipaddrListTest(ipaddrs);
+        // Write the struct
+        wxfer = wjson_struct_test_.write(prot);
+        // Verify Default values
+        EXPECT_EQ(true, wjson_struct_test_.get_uuidDefaultTest() ==
+             boost::uuids::string_generator()
+             ("00010203-0405-0607-0423-023434265323"));
+        // Get the buffer
+        btrans->getBuffer(&buffer, &offset);
+        uint8_t stream[4096];
+        uint8_t *data = stream;
+        std::string data_s;
+        data_s.append((char *)buffer, offset);
+    	std::cout << " buffer len " << offset << std::endl;
+    	std::cout << " buffer val " << data_s << std::endl;
+        EXPECT_EQ(wxfer, offset);
+    }
+
     SandeshStructTest wstruct_test_;
     SandeshStructTest rstruct_test_;
+    SandeshStructJsonTest wjson_struct_test_;
 };
 
 
@@ -146,6 +227,17 @@ TEST_F(SandeshReadWriteUnitTest, StructBinaryReadWrite) {
     SandeshReadWriteProcess(btrans, prot);
 }
 
+
+TEST_F(SandeshReadWriteUnitTest, StructJSONReadWrite) {
+    boost::shared_ptr<TMemoryBuffer> btrans =
+            boost::shared_ptr<TMemoryBuffer>(
+                    new TMemoryBuffer(4096));
+    boost::shared_ptr<TJSONProtocol> prot =
+            boost::shared_ptr<TJSONProtocol>(
+                    new TJSONProtocol(btrans));
+    prot->setSandeshEnd(false);
+    SandeshJsonReadWriteProcess(btrans, prot);
+}
 
 class SandeshLogUnitTest : public ::testing::Test {
 protected:
