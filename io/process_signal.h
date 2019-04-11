c9a653cc82937fbfeef5230688b5a8d216791526
Mateusz Jablonski <mateusz.jablonski@codilime.com>
2019-04-11 16:16:51 +0200

    Fix warning C4065 in process_signal for Windows
    1. Fix `Switch statement contains 'default' but no 'case' labels`.
    2. Move all OS-specific code to platform-specific files.
    
    Change-Id: I653dffc1fd01ce5caecc1048da429ecaa9cb2ee1
    Partial-Bug: #1800894

--- io/process_signal.h
+++ io/process_signal.h
@@ -39,8 +39,11 @@ class Signal {
     void RegisterHandler(SignalChildHandler handler);
 
  private:
+    boost::system::error_code AddSignal(int sig);
+    boost::system::error_code InitializeSigChld();
     void RegisterSigHandler();
     void HandleSig(const boost::system::error_code& error, int sig);
+    bool HandleSigOsSpecific(const boost::system::error_code& error, int sig);
     void NotifySig(const boost::system::error_code &error, int sig);
     void NotifySigChld(const boost::system::error_code &error, int sig, int pid,
                        int status);
---
+++ io/windows/process_signal.cc
@@ -0,0 +1,22 @@
+//
+// Copyright (c) 2019 Juniper Networks, Inc. All rights reserved.
+//
+
+#include <boost/system/error_code.hpp>
+
+#include <io/process_signal.h>
+
+namespace process {
+
+boost::system::error_code Signal::InitializeSigChld() {
+    return boost::system::error_code();
+}
+
+void Signal::RegisterHandler(SignalChildHandler handler) {}
+
+bool Signal::HandleSigOsSpecific(const boost::system::error_code& error,
+                                 int sig) {
+    return false;
+}
+
+} // namespace process
